34b4466c77797db02833bce52d409cd47f6245e8
williamjiamin <lipai_wang@163.com>
2021-03-03 15:21:26 +0800

    init commit

---
+++ app/src/main/java/com/lexueoude/helloworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lexueoude.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
